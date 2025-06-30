668fccd192d6285888878cc74f2c40322562d175
deniol54 <daniil.paholkov@yandex.ru>
2025-06-30 20:28:10 +0300

    Реализованы и подключены логгеры

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { TskvLogger } from './logger/tskv.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   app.setGlobalPrefix('api/afisha');
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.useLogger(new TskvLogger());
   await app.listen(3000);
 }
 bootstrap();
